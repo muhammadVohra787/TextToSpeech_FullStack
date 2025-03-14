b017d96dd07caac444e1a8a4016fdee0ab3a0c47
muhammadVohra787 <mvohra10@my.centennialcollege.ca>
2025-03-13 20:14:28 -0400

    forgot, change password and get user added

--- server/users/user_controller.py
+++ server/users/user_controller.py
@@ -64,7 +64,74 @@ def login_user(request):
         
 
 
+@csrf_exempt
+def forgot_password(request):
+    if request.method == "POST":
+        try:
+            data = json.loads(request.body)
+            email = data["email"]
+            sq1=data["sq1"]
+            sa1=data["sa1"]
+            sq2=data["sq2"]
+            sa2=data["sa2"]
+            password = data["password"]
+            
+            user = User.objects.filter(email= email).first()
+            if user is None:
+                return JsonResponse({"message": "Invalid credentials, Check your email password"}, status=401)
+            
+            if user.sa1 == sa1 and user.sa2 ==sa2:
+                user.password = make_password(password)
+                user.save()
+                
+            return JsonResponse({"message": "Password changed", "success": True}, status=201)
+        except Exception as e:
+            return JsonResponse({"message": str(e)}, status=400)
+    else:
+        return JsonResponse({"message": "Invalid request method"}, status=405)        
+ 
+@csrf_exempt
+def reset_password(request):
+    if request.method == "POST":
+        try:
+            data = json.loads(request.body)
+            email = data["email"]
+            old_password = data["oldPassword"]
+            new_password = data["newPassword"]
+            user = User.objects.filter(email= email).first()
+            
+            if not user or not check_password(old_password, user.password):
+                return JsonResponse({"message": "Invalid credentials, Check your email password"}, status=401)
+            user.password = make_password(new_password)
+            user.save()
+                
+            return JsonResponse({"message": "Password changed", "success": True}, status=201)
+        except Exception as e:
+            return JsonResponse({"message": str(e)}, status=400)
+    else:
+        return JsonResponse({"message": "Invalid request method"}, status=405)        
+    
+     
+     
+@csrf_exempt
+def get_user(request, user_id):
+    if request.method != "GET":
+        return JsonResponse({"message": "Invalid request method"}, status=405)
 
+    try:
+        user = User.objects.filter(_id=user_id).values(
+            "id", "fullName", "email", "sq1", "sa1", "sq2", "sa2", "admin"
+        ).first()  # Excludes password
+            
+        if not user:
+            return JsonResponse({"message": "User not found"}, status=404)
+
+        return JsonResponse({"user": user}, status=200)
+
+    except Exception as e:
+        return JsonResponse({"message": str(e)}, status=500)
+      
+             
 class MongoJSONEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, ObjectId):
