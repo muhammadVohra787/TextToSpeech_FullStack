4da97c75871a1a6de60610f88b5bb08c13776680
muhammadVohra787 <mvohra10@my.centennialcollege.ca>
2025-03-15 00:53:27 -0400

    fixed the wrong merch with AI-side

--- TTS-AI/tts_project/tts_app/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class ProcessedText(models.Model):
-    sentence = models.TextField(unique=True)
-    mp3_path = models.FileField(upload_to='generated_audio/')
-
-    def __str__(self):
-        return self.sentence
--- TTS-AI/tts_project/tts_app/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import ProcessedText
-
-class ProcessedTextSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ProcessedText
-        fields = ['sentence', 'mp3_path']
