681aa9e9e7e0c60d7b39e5ca0fde00b8854ef7f6
muhammadVohra787 <prof.ahmedvohra787@gmail.com>
2025-04-16 16:26:18 -0400

    minor bug

--- server/myproject/urls.py
+++ server/myproject/urls.py
@@ -5,6 +5,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/users/", include("users.user_urls")),
-    path("api/storage/", include("storage.storage_urls")),
     path('api/tts/', include('tts.tts_urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
