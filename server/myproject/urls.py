071e3d10310416c2115a4cd03aee7a639085bf8d
muhammadVohra787 <Git123@456@789@>
2025-08-10 17:56:45 -0400

    added health url

--- server/myproject/urls.py
+++ server/myproject/urls.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+def home(request):
+    return HttpResponse("Backend is running ✅")
+
+
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path("api/users/", include("users.user_urls")),
     path('api/tts/', include('tts.tts_urls'))
