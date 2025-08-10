c43e6bdf1488b3d98d0e2e8fd435b2c5d5761c50
muhammadVohra787 <Git123@456@789@>
2025-08-10 12:05:13 -0400

    added url in allowed host for more security

--- server/myproject/settings.py
+++ server/myproject/settings.py
@@ -28,7 +28,11 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    "texttospeech-fullstack.onrender.com",
+    "localhost",
+    "127.0.0.1"
+]
 
 
 # Application definition
